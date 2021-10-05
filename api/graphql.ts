4f50e96bfad9e808bc6a96da15b872778cdfe1ab
Ella N <38847123+ellanan@users.noreply.github.com>
2021-10-05 00:38:27 -0400

    generate schema from server makeSchema

--- api/graphql.ts
+++ api/graphql.ts
@@ -1,11 +1,16 @@
 import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import { ApolloServer } from 'apollo-server-micro';
 import { makeSchema } from 'nexus';
+import { join } from 'path';
 import * as mutationTypes from './graphqlTypes/mutationTypes';
 import * as queryTypes from './graphqlTypes/queryTypes';
 
 const schema = makeSchema({
   types: [...Object.values(queryTypes), ...Object.values(mutationTypes)],
+  outputs: {
+    typegen: join(__dirname, '..', 'nexus-typegen.ts'), // 2
+    schema: join(__dirname, '..', 'schema.graphql'), // 3
+  },
 });
 
 const server = new ApolloServer({
