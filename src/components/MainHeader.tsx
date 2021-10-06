4c7e00fa80a8101b898c38a6559d3d9ba76578f1
Ella N <38847123+ellanan@users.noreply.github.com>
2021-10-06 16:32:58 -0400

    add styling to main header

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,30 +1,49 @@
-// /** @jsxImportSource @emotion/react */
-// import { css } from '@emotion/react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 import { NavLink } from 'react-router-dom';
+
+const Li = styled.li`
+  padding: 1em;
+`;
 
 export const MainHeader = () => {
   return (
-    <nav>
+    <nav
+      css={css`
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+      `}
+    >
       <h1>MealKit</h1>
-      <ul>
-        <li>
+      <ul
+        css={css`
+          display: flex;
+          flex-direction: row;
+          align-items: center;
+          justify-content: flex-end;
+          width: 100%;
+        `}
+      >
+        <Li>
           <NavLink to='/'>home</NavLink>
-        </li>
-        <li>
+        </Li>
+        <Li>
           <NavLink to='/createrecipe'>create recipe</NavLink>
-        </li>
-        <li>
+        </Li>
+        <Li>
           <NavLink to='/recipes'>recipes</NavLink>
-        </li>
-        <li>
+        </Li>
+        <Li>
           <NavLink to='/mealplan'>meal plan</NavLink>
-        </li>
-        <li>
+        </Li>
+        <Li>
           <NavLink to='/grocerylist'>grocery list</NavLink>
-        </li>
-        <li>
+        </Li>
+        <Li>
           <NavLink to='/login'>login</NavLink>
-        </li>
+        </Li>
       </ul>
     </nav>
   );
