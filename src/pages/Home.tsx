03c001c52b13c189141b029aa533c85e8c4039f6
Ella N <38847123+ellanan@users.noreply.github.com>
2021-10-06 16:33:16 -0400

    add search form on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,14 @@ export const Home = () => {
   return (
     <>
       <h1>home page</h1>
+      <form>
+        <input
+          type='search'
+          name='search recipe'
+          placeholder='search recipes'
+        ></input>
+        <button>search</button>
+      </form>
     </>
   );
 };
