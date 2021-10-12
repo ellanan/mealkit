374193be1ddef00a96fd31bfeba45ddc1490538d
Ella N <38847123+ellanan@users.noreply.github.com>
2021-10-12 11:22:32 -0400

    add meal and render added meal on meal plan page

--- src/pages/MealPlan.tsx
+++ src/pages/MealPlan.tsx
@@ -1,11 +1,22 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
 import { DateTime, Interval } from 'luxon';
 import { useQuery, gql } from '@apollo/client';
-import type * as GraphQLTypes from '../generated/graphql';
-import { Fragment } from 'react';
+import * as GraphQLTypes from '../generated/graphql';
+import { Fragment, useState } from 'react';
+
+import { AddRecipeToMealPlanForm } from '../components/AddRecipeToMealPlanForm';
+
+// @ts-ignore
+window.DateTime = DateTime;
 
 export const MealPlan = () => {
+  const [mealTypeAndDate, setMealTypeAndDate] = useState<{
+    mealType: GraphQLTypes.MealType | null;
+    date: string | null;
+  }>({
+    mealType: null,
+    date: null,
+  });
+
   const startDate = DateTime.now().startOf('week');
   const endDate = startDate.endOf('week');
 
@@ -50,19 +61,13 @@ export const MealPlan = () => {
   return (
     <>
       <h1>meal plan</h1>
-      <div
-        css={css`
-          display: flex;
-          flex-direction: column;
-        `}
-      >
-        <h2>
-          {interval[0].monthLong} {interval[0].year}
-        </h2>
-      </div>
+      <h2>
+        {interval[0].monthLong} {interval[0].year}
+      </h2>
       <table>
         <thead>
           <tr>
+            <th></th>
             {interval.map((day) => (
               <th key={day.toISO()}>
                 <span>{day.weekdayShort}</span>
@@ -72,35 +77,57 @@ export const MealPlan = () => {
           </tr>
         </thead>
         <tbody>
-          {['BREAKFAST', 'LUNCH', 'DINNER'].map((mealType) => (
-            <Fragment key={mealType}>
-              <tr>
-                <td colSpan={interval.length}>{mealType}</td>
-              </tr>
-              <tr>
-                {interval.map((day) => {
-                  const mealPlanEntries =
-                    data?.currentUser?.mealPlan?.schedule.filter((entry) => {
-                      return (
-                        entry?.mealType === mealType &&
-                        entry.date &&
-                        day.toISODate() ===
-                          DateTime.fromISO(entry.date).toISODate()
-                      );
-                    });
-                  return (
-                    <td key={day.toISO()}>
-                      {mealPlanEntries?.map((entry) => (
-                        <div key={entry?.id}>{entry?.recipe?.name}</div>
-                      ))}
-                    </td>
-                  );
-                })}
-              </tr>
-            </Fragment>
+          {[
+            GraphQLTypes.MealType.Breakfast,
+            GraphQLTypes.MealType.Lunch,
+            GraphQLTypes.MealType.Dinner,
+          ].map((mealType) => (
+            <tr key={mealType}>
+              <td valign='top'>{mealType}</td>
+              {interval.map((day) => {
+                const mealPlanEntries =
+                  data?.currentUser?.mealPlan?.schedule.filter((entry) => {
+                    return (
+                      entry?.mealType === mealType &&
+                      entry.date &&
+                      day.toISODate() ===
+                        DateTime.fromISO(entry.date, {
+                          setZone: true,
+                        }).toISODate()
+                    );
+                  });
+                return (
+                  <td key={day.toISO()} valign='top'>
+                    <button
+                      onClick={() => {
+                        setMealTypeAndDate({
+                          mealType,
+                          date: day.toISODate(),
+                        });
+                      }}
+                    >
+                      add recipe to meal
+                    </button>
+                    {mealPlanEntries?.map((entry) => (
+                      <div key={entry?.id}>{entry?.recipe?.name}</div>
+                    ))}
+                  </td>
+                );
+              })}
+            </tr>
           ))}
         </tbody>
       </table>
+      {JSON.stringify({ mealTypeAndDate })}
+      {data?.currentUser?.mealPlan?.id &&
+        mealTypeAndDate?.date &&
+        mealTypeAndDate?.mealType && (
+          <AddRecipeToMealPlanForm
+            mealPlanId={data?.currentUser?.mealPlan?.id}
+            date={mealTypeAndDate.date}
+            mealType={mealTypeAndDate.mealType}
+          />
+        )}
     </>
   );
 };
