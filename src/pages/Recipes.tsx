b5528e9f60cd8e416f755c9c93c6f8f7d06f923f
Ella N <38847123+ellanan@users.noreply.github.com>
2021-10-21 23:42:37 -0400

    update input with chakra input and update styles in recipes page

--- src/pages/Recipes.tsx
+++ src/pages/Recipes.tsx
@@ -5,6 +5,7 @@ import type * as GraphQLTypes from '../generated/graphql';
 import { Search2Icon } from '@chakra-ui/icons';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { Input, InputGroup, InputLeftElement } from '@chakra-ui/input';
 
 const defaultImg = require('../images/defaultImg.jpg').default;
 
@@ -38,50 +39,11 @@ export const Recipes = () => {
           justify-content: space-between;
         `}
       >
-        <div
-          css={css`
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-            border: 0.5px solid #ebb59c;
-            border-radius: 20px;
-            height: 2.4em;
-            padding: 16px;
-            width: 40%;
-            margin-top: 3rem;
-          `}
-        >
-          <label htmlFor='name'>
-            <Search2Icon
-              css={css`
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                height: 15px;
-                width: 15px;
-                margin-right: 0.5em;
-                left: 0;
-                color: #ebb59c;
-              `}
-            />
-          </label>
-          <input
-            type='text'
-            placeholder='search recipe'
-            size={20}
-            css={css`
-              outline: none;
-            `}
-            onChange={(e) => {
-              e.preventDefault();
-              setSearch(e.target.value);
-            }}
-          />
-        </div>
         <NavLink
           to='/recipes/create-recipe'
           css={css`
             padding: 0.5em 1.2em;
+            margin-top: 2rem;
             color: #fff;
             font-weight: 700;
             border-radius: 20px;
@@ -90,16 +52,47 @@ export const Recipes = () => {
         >
           create recipe
         </NavLink>
+        <InputGroup
+          borderColor='#ebb59c'
+          css={css`
+            & ::placeholder {
+              color: #555;
+            }
+          `}
+        >
+          <InputLeftElement
+            pointerEvents='none'
+            ml='2'
+            children={
+              <Search2Icon
+                css={css`
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                  height: 15px;
+                  width: 15px;
+                  margin-right: 0.5em;
+                  left: 0;
+                  color: #ebb59c;
+                `}
+              />
+            }
+          />
+          <Input
+            type='tel'
+            color='#fff'
+            placeholder='Search for recipe'
+            borderRadius='20px'
+            onChange={(e) => {
+              e.preventDefault();
+              setSearch(e.target.value);
+            }}
+          />
+        </InputGroup>
       </div>
       <div
         css={css`
-          display: grid;
-          grid-template-columns: 1fr;
-          align-items: center;
-          justify-content: flex-start;
-          margin-top: 2rem;
-          margin-right: 0;
-          width: 12rem;
+          margin-top: 1rem;
         `}
       >
         {data?.recipes
@@ -107,49 +100,49 @@ export const Recipes = () => {
             recipe.name.toLowerCase().includes(search.toLowerCase())
           )
           .map((recipe) => (
-            <div key={recipe.id}>
-              <NavLink
-                to={`/recipes/${recipe.id}`}
-                css={css`
-                  display: flex;
-                  flex-direction: column;
-                  align-items: center;
-                  justify-content: center;
-                  padding: 0.4em;
-                  color: #593e31;
-                  border-radius: 20px;
+            <NavLink
+              to={`/recipes/${recipe.id}`}
+              css={css`
+                display: flex;
+                align-items: center;
+                padding: 0.4em 1em;
+                color: #593e31;
 
-                  :hover {
-                    background-color: #f8c9b3;
-                  }
-                `}
-              >
-                {recipe.imageUrl ? (
-                  <img
-                    src={`${recipe.imageUrl}`}
-                    alt=''
-                    css={css`
-                      width: 4rem;
-                      height: 3rem;
-                      border-radius: 20px;
-                      margin-right: 0.5rem;
-                    `}
-                  />
-                ) : (
-                  <img
-                    src={defaultImg}
-                    alt=''
-                    css={css`
-                      width: 4rem;
-                      height: 3rem;
-                      border-radius: 20px;
-                      margin-right: 0.5rem;
-                    `}
-                  />
-                )}
-                {recipe.name}
-              </NavLink>
-            </div>
+                :hover {
+                  background-image: linear-gradient(
+                    to right,
+                    transparent,
+                    #fae4daa3 20%,
+                    #fae4da
+                  );
+                }
+              `}
+            >
+              {recipe.imageUrl ? (
+                <img
+                  src={`${recipe.imageUrl}`}
+                  alt=''
+                  css={css`
+                    width: 4rem;
+                    height: 3rem;
+                    border-radius: 20px;
+                    margin-right: 0.5rem;
+                  `}
+                />
+              ) : (
+                <img
+                  src={defaultImg}
+                  alt=''
+                  css={css`
+                    width: 4rem;
+                    height: 3rem;
+                    border-radius: 20px;
+                    margin-right: 0.5rem;
+                  `}
+                />
+              )}
+              {recipe.name}
+            </NavLink>
           ))}
       </div>
     </div>
