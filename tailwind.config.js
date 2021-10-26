9ca054376779fb680a18dc44c0678b18b854d67e
Ella N <38847123+ellanan@users.noreply.github.com>
2021-10-26 16:53:25 -0400

    add custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,27 @@ module.exports = {
   purge: ['./public/index.html', './src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        11: '#7e7979',
+        12: '#f3f0ed',
+        13: '#646161',
+        14: '#593e31',
+        15: '#5c5656',
+        16: '#383636',
+        17: '#',
+        18: '#',
+        19: '#',
+        21: '#',
+        22: '#',
+        23: '#',
+        24: '#',
+        25: '#',
+      },
+      boxShadow: {
+        none: 'none',
+      },
+    },
   },
   variants: {
     extend: {},
