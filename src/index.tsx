6ae3c0627371f3987135152fd8d89d76c2260464
Ella N <38847123+ellanan@users.noreply.github.com>
2021-11-11 23:12:02 -0500

    add audience to react auth0provider

--- src/index.tsx
+++ src/index.tsx
@@ -21,6 +21,7 @@ ReactDOM.render(
           domain={process.env.REACT_APP_AUTH0_DOMAIN as string}
           clientId={process.env.REACT_APP_AUTH0_CLIENT_ID as string}
           redirectUri={window.location.origin}
+          audience={process.env.REACT_APP_AUTH0_AUDIENCE}
         >
           <AuthAccessTokenProvider>
             <ApolloClientProvider>
