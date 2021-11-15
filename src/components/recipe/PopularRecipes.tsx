680a8f78aec1283a8e4120a4e84fd5a35f0f8cf1
Ella N <38847123+ellanan@users.noreply.github.com>
2021-11-15 15:37:29 -0500

    add navlink to recipes under popular recipes

--- src/components/recipe/PopularRecipes.tsx
+++ src/components/recipe/PopularRecipes.tsx
@@ -1,9 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { DateTime } from 'luxon';
 import { useQuery, gql } from '@apollo/client';
 import * as GraphQLTypes from '../../generated/graphql';
 import { useMemo } from 'react';
 import { BsInfoCircle as InfoIcon } from 'react-icons/bs';
 import { Tooltip } from '@chakra-ui/react';
+import { NavLink } from 'react-router-dom';
+import { Spinner } from '@chakra-ui/spinner';
 
 const defaultImg = require('../mealPlan/images/defaultImg.jpg').default;
 
@@ -42,8 +46,8 @@ export const PopularRecipes = () => {
   }
 
   return (
-    <div className='py-1 px-4 text-14'>
-      <h1 className='text-sm text-25 font-medium mb-1'>
+    <>
+      <h1 className='text-sm text-25 font-medium mb-1 py-1 px-4'>
         <Tooltip
           label='Based on frequency from the last 30 days'
           className='bg-gray-400 bg-opacity-90 p-2 font-Raleway'
@@ -53,23 +57,47 @@ export const PopularRecipes = () => {
           </span>
         </Tooltip>
       </h1>
-      <span className='text-sm'>
-        <ol>
-          {data?.currentUser?.mealPlan?.popularRecipes.map((recipe) => (
-            <li
-              className='flex flex-row items-center justify-start relative overflow-hidden'
-              key={recipe.id}
-            >
+      <div className='flex flex-col overflow-auto'>
+        {data?.currentUser?.mealPlan?.popularRecipes.map((recipe) => (
+          <NavLink
+            className='flex items-center py-1 px-4 text-14 text-sm'
+            key={recipe.id}
+            to={(location) => {
+              const newQueryParams = new URLSearchParams(location.search);
+              newQueryParams.set('modalRecipeId', recipe.id);
+
+              return {
+                ...location,
+                search: newQueryParams.toString(),
+              };
+            }}
+            css={css`
+              :hover {
+                background-image: linear-gradient(
+                  to right,
+                  transparent,
+                  #fae4daa3 20%,
+                  #fae4da
+                );
+              }
+            `}
+          >
+            <div className='w-12 h-12 rounded-full mr-2 relative overflow-hidden flex-shrink-0'>
               <img
-                className='object-cover w-12 h-12 rounded-full mb-1 mr-2'
-                src={recipe.imageUrl || defaultImg}
+                className='object-cover w-full h-full'
+                src={recipe.imageUrl ?? defaultImg}
                 alt=''
               />
-              {recipe.name}
-            </li>
-          ))}
-        </ol>
-      </span>
-    </div>
+              {recipe.id.startsWith('temp-id') && (
+                <div className='flex items-center justify-center w-full h-full absolute top-0 bottom-0 backdrop-filter backdrop-blur-sm'>
+                  <Spinner size='sm' color='#f88c5a' />
+                </div>
+              )}
+            </div>
+            {recipe.name}
+          </NavLink>
+        ))}
+      </div>
+    </>
   );
 };
