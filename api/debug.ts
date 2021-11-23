990b12b47038fa799d43af4e0bcbd1128a283528
Ella N <38847123+ellanan@users.noreply.github.com>
2021-11-23 18:01:51 -0500

    match prisma version with @prisma/client

--- api/debug.ts
+++ api/debug.ts
@@ -1,10 +1,10 @@
 import type { Handler } from 'express';
 import { prisma } from './_helpers/prismaClient';
 
-const debugHandler: Handler = (req, res) => {
+const debugHandler: Handler = async (req, res) => {
   const cursor = req.query.cursor as string;
   console.log('debug:cursor', cursor);
-  prisma.recipe.findMany({
+  const results = await prisma.recipe.findMany({
     orderBy: {
       updatedAt: 'desc',
     },
@@ -12,5 +12,7 @@ const debugHandler: Handler = (req, res) => {
     skip: cursor ? 1 : 0,
     cursor: cursor ? { id: cursor } : undefined,
   });
+
+  res.json(results);
 };
 export default debugHandler;
