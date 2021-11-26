b17613860838aa18f0857c28c0f62cb5720209a2
Ella N <38847123+ellanan@users.noreply.github.com>
2021-11-25 22:29:52 -0500

    add home button

--- src/components/mobile/MobileBottomNavbar.tsx
+++ src/components/mobile/MobileBottomNavbar.tsx
@@ -2,10 +2,19 @@ import { NavLink } from 'react-router-dom';
 import { MdCreate } from 'react-icons/md';
 import { GoSearch } from 'react-icons/go';
 import { BiListUl } from 'react-icons/bi';
+import { AiOutlineHome } from 'react-icons/ai';
 
 export const MobileBottomNavbar = () => {
   return (
     <div className='flex items-center justify-evenly bg-24 py-3 px-4 font-Raleway'>
+      <NavLink
+        className='flex flex-col items-center w-2/12 text-14 text-[10px] mx-2 px-2 font-medium uppercase'
+        to='/'
+      >
+        <AiOutlineHome className='mr-1 mb-1' size={20} />
+        Home
+      </NavLink>
+
       <NavLink
         className='flex flex-col items-center w-2/12 text-14 text-[10px] mx-2 px-2 font-medium uppercase'
         to='/recipes'
