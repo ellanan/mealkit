3d55001927b2c272f1b0d1cf7aae6263587c823b
Ella N <38847123+ellanan@users.noreply.github.com>
2021-11-29 13:55:06 -0500

    update styles on mobile home page

--- src/components/mobile/MobileHome.tsx
+++ src/components/mobile/MobileHome.tsx
@@ -11,7 +11,7 @@ export const MobileHome = () => {
 
   return (
     <div className='flex flex-col h-full w-full font-Montserrat'>
-      <div className='flex items-center pl-12 pr-8 py-6'>
+      <div className='flex items-center p-6'>
         <NavLink
           to='/'
           className='text-3xl font-medium text-23 font-Montserrat flex'
@@ -27,19 +27,19 @@ export const MobileHome = () => {
         </button>
       </div>
 
-      <h1 className='text-14 text-5xl text-center font-extrabold font-Vollkorn px-4 pt-6 pb-12 mt-10 max-w-[19em] mx-auto'>
+      <h1 className='text-14 text-4xl text-center font-extrabold font-Vollkorn px-4 pt-4 pb-12 mt-10 max-w-[19em] mx-auto'>
         Don't let food be something that "happens to you"
       </h1>
 
       <div className='flex items-center justify-center relative text-center mb-20'>
         <MainBlob className='absolute w-3/4 -top-[-16.5rem]' />
         <img
-          className='w-5/12 relative'
+          className='w-9/12 relative'
           src={require('../../images/healthy-vegetables.png').default}
           alt='healthy vegetables'
         />
-        <span className='absolute text-14 text-3xl top-2/4 left-2/4 -translate-x-2/4 -translate-y-1/4'>
-          What's for <br />
+        <span className='absolute text-14 text-base top-2/4 left-2/4 -translate-x-2/4 -translate-y-1/4'>
+          What's for
           <Typewritter
             options={{
               strings: ['breakfast?', 'lunch?', 'dinner?'],
@@ -50,9 +50,9 @@ export const MobileHome = () => {
         </span>
       </div>
 
-      <div className='flex items-center justify-between my-6 mx-28'>
+      <div className='flex flex-col items-center justify-center my-6 mx-10'>
         <div className='max-w-[320px]'>
-          <h1 className='text-14 text-4xl font-bold pb-5'>
+          <h1 className='text-14 text-2xl font-bold pb-4'>
             Plan meals for the week
           </h1>
           <p className='text-lg'>
@@ -61,20 +61,15 @@ export const MobileHome = () => {
           </p>
         </div>
         <img
-          className='mt-6 w-6/12 rounded-xl'
+          className='mt-6 w-10/12 rounded-xl'
           src={require('../../images/meal-plan-example.jpg').default}
           alt=''
         />
       </div>
 
-      <div className='flex items-center justify-between my-6 mx-28'>
-        <img
-          className='mt-6 w-6/12 rounded-xl'
-          src={require('../../images/groceries-list-example.jpg').default}
-          alt=''
-        />
+      <div className='flex flex-col items-center justify-center my-6 mx-10'>
         <div className='max-w-[320px]'>
-          <h1 className='text-14 text-4xl font-bold pb-5'>
+          <h1 className='text-14 text-2xl font-bold pb-4'>
             Auto generate grocery list
           </h1>
           <p className='text-lg'>
@@ -82,11 +77,16 @@ export const MobileHome = () => {
             selected date range.
           </p>
         </div>
+        <img
+          className='mt-6 w-10/12 rounded-xl'
+          src={require('../../images/groceries-list-example.jpg').default}
+          alt=''
+        />
       </div>
 
-      <div className='flex items-center justify-between my-6 mx-28'>
+      <div className='flex flex-col items-center justify-center my-6 mx-10'>
         <div className='max-w-[320px]'>
-          <h1 className='text-14 text-4xl font-bold pb-5'>
+          <h1 className='text-14 text-2xl font-bold pb-4'>
             Collaborate with family and friends
           </h1>
           <p className='text-lg'>
@@ -95,7 +95,7 @@ export const MobileHome = () => {
           </p>
         </div>
         <img
-          className='mt-6 w-6/12 rounded-xl'
+          className='mt-6 w-10/12 rounded-xl'
           src={require('../../images/share-plan-example.svg').default}
           alt=''
         />
@@ -107,41 +107,36 @@ export const MobileHome = () => {
           alt=''
           className='transform -translate-y-full absolute top-[1px]'
         />
-        <h1 className='text-14 text-sm font-semibold mt-10'>ATTRIBUTIONS</h1>
-        <div className='flex items-center'>
-          <ul className='p-2 text-xs text-14'>
+        <h1 className='text-14 text-sm font-semibold mt-8'>ATTRIBUTIONS</h1>
+        <div className='flex items-center justify-start'>
+          <ul className='text-xs text-14 mt-4'>
             <li className='flex items-center'>
-              <CarrotLogo className='w-5 mb-4' /> | Carrot logo created by Free
-              ICONS Library |
               <a
-                className='text-14 hover:text-25 hover:underline'
+                className='text-14 px-4 py-2 hover:text-25 hover:underline'
                 href='https://icon-library.com/840638.svg.html'
-                target='_blank'
-                rel='noreferrer'
               >
-                https://icon-library.com/840638.svg.html
+                1. Carrot logo created by Free ICONS Library
               </a>
             </li>
-            <li>attribution</li>
-            <li>attribution</li>
-            <li>attribution</li>
-          </ul>
-          <ul className='p-2 text-xs text-14'>
+
             <li className='flex items-center'>
-              <CarrotLogo className='w-5 mb-4' /> Carrot logo created by Free
-              ICONS Library
               <a
-                className='text-14 hover:text-25 hover:underline'
-                href='https://icon-library.com/840638.svg.html'
-                target='_blank'
-                rel='noreferrer'
+                className='text-14 px-4 py-2 hover:text-25 hover:underline'
+                href='https://www.freepik.com/photos/background'
               >
-                https://icon-library.com/840638.svg.html
+                2. Healthy vegetables created by jcomp - www.freepik.com
               </a>
             </li>
-            <li>attribution</li>
-            <li>attribution</li>
-            <li>attribution</li>
+
+            <li className='flex items-center'>
+              <a
+                className='text-14 px-4 py-2 hover:text-25 hover:underline'
+                href='https://www.freepik.com/vectors/food'
+              >
+                3. Family cooking together created by pch.vector -
+                www.freepik.com
+              </a>
+            </li>
           </ul>
         </div>
         <Footer />
