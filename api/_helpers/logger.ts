e8d7ffcafbb56fde9894dbcb0a6b7d41895405ee
Ella N <38847123+ellanan@users.noreply.github.com>
2021-12-01 01:12:04 -0500

    try fixing papertrail
    logs aren't showing up

--- api/_helpers/logger.ts
+++ api/_helpers/logger.ts
@@ -3,7 +3,12 @@ import winston from 'winston';
 const papertrail = new winston.transports.Http({
   host: 'logs.collector.solarwinds.com',
   path: '/v1/log',
-  auth: { username: '', password: process.env.PAPERTRAIL_TOKEN },
+  auth: {
+    // @ts-expect-error not sure why papertrail is not working, their example shows new String(''), shouldn't matter but need to try
+    // eslint-disable-next-line no-new-wrappers
+    username: new String(''),
+    password: process.env.PAPERTRAIL_TOKEN,
+  },
   ssl: true,
 });
 
