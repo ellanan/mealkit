90d4750e7c79d18d1ef2aa8234b243a3d0c051f9
Ella N <38847123+ellanan@users.noreply.github.com>
2021-12-01 01:54:23 -0500

    catch error from beforeExist

--- api/_helpers/prismaClient.ts
+++ api/_helpers/prismaClient.ts
@@ -17,6 +17,12 @@ prisma.$on('query', async (e) => {
   console.log(`${e.query} ${e.params}`);
 });
 
-prisma.$on('beforeExit', async () => {
-  logger.info(`${instanceId}: Prisma client disconnecting.`);
-});
+try {
+  // prisma data proxy doesn't seem to support beforeExit yet
+  // this throws "ERROR	NotImplementedYetError: beforeExit event is not yet supported"
+  prisma.$on('beforeExit', async () => {
+    logger.info(`${instanceId}: Prisma client disconnecting.`);
+  });
+} catch (e) {
+  console.log(e);
+}
