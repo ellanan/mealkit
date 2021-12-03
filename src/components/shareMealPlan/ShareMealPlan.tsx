1b19b5eb73f64daf7f6702de0626415d7d76f0de
Ella N <38847123+ellanan@users.noreply.github.com>
2021-12-02 23:00:54 -0500

    update share meal plan component to only handle navlink

--- src/components/shareMealPlan/ShareMealPlan.tsx
+++ src/components/shareMealPlan/ShareMealPlan.tsx
@@ -1,50 +1,24 @@
-import { useQuery, gql } from '@apollo/client';
-import * as GraphQLTypes from '../../generated/graphql';
 import { BiShareAlt } from 'react-icons/bi';
-import { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 
 export const ShareMealPlan = () => {
-  const [copySuccessMessage, setCopySuccessMessage] = useState('');
-
-  const { data, error: errorLoadingUserMealPlan } =
-    useQuery<GraphQLTypes.CurrentUserMealPlanQuery>(gql`
-      query CurrentUserMealPlan {
-        currentUser {
-          id
-          mealPlan {
-            id
-          }
-        }
-      }
-    `);
-  if (errorLoadingUserMealPlan) {
-    throw errorLoadingUserMealPlan;
-  }
-
-  const mealPlanUrl = `${window.location.origin}/invite?mealPlanId=${data?.currentUser?.mealPlan?.id}`;
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setCopySuccessMessage('');
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, [copySuccessMessage]);
-
   return (
     <div className='flex flex-col'>
-      <p className='text-sm text-green-400 min-h-[20px] ml-11'>
-        {copySuccessMessage}
-      </p>
-      <button
+      <NavLink
         className='font-Raleway flex items-center text-14 text-sm pl-4 py-2 hover:bg-12'
-        onClick={() => {
-          navigator.clipboard.writeText(mealPlanUrl);
-          setCopySuccessMessage('Copied to clipboard!');
+        to={(location) => {
+          const newQueryParams = new URLSearchParams(location.search);
+          newQueryParams.append('modalShareMealPlan', 'visible');
+
+          return {
+            ...location,
+            search: newQueryParams.toString(),
+          };
         }}
       >
         <BiShareAlt size={14} className='min-w-[20px] mr-2' />
         Share Meal Plan
-      </button>
+      </NavLink>
     </div>
   );
 };
