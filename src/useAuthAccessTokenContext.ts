a3234f006b95cfa8dc9496a54d32e33eeec3ac9d
Ella N <38847123+ellanan@users.noreply.github.com>
2021-12-13 23:40:16 -0500

    check for errors

--- src/useAuthAccessTokenContext.ts
+++ src/useAuthAccessTokenContext.ts
@@ -1,24 +1,45 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import constate from 'constate';
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const useAuthAccessToken = () => {
-  const { isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
+  const {
+    isAuthenticated,
+    isLoading,
+    getAccessTokenSilently,
+    loginWithRedirect,
+  } = useAuth0();
   const [accessToken, setAccessToken] = useState<string>();
 
   const [isGettingAccessToken, setIsGettingAccessToken] =
     useState<boolean>(false);
+
+  const trySetAccessToken = useCallback(
+    async function getAccessToken() {
+      if (isAuthenticated) {
+        setIsGettingAccessToken(true);
+        try {
+          const token = await getAccessTokenSilently();
+          setAccessToken(token);
+          setIsGettingAccessToken(false);
+        } catch (e) {
+          if (e === 'login_required') {
+            loginWithRedirect();
+          } else if (e === 'consent_required') {
+            loginWithRedirect();
+          } else {
+            setAccessToken(undefined);
+            throw e;
+          }
+        }
+      }
+    },
+    [getAccessTokenSilently, isAuthenticated, loginWithRedirect]
+  );
+
   useEffect(() => {
-    if (isAuthenticated) {
-      setIsGettingAccessToken(true);
-      getAccessTokenSilently().then((accessToken) => {
-        setIsGettingAccessToken(false);
-        setAccessToken(accessToken);
-      });
-    } else {
-      setAccessToken(undefined);
-    }
-  }, [getAccessTokenSilently, isAuthenticated]);
+    trySetAccessToken();
+  }, [trySetAccessToken]);
 
   return {
     accessToken,
