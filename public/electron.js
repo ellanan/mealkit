918cf54a35b6312bb33afe17f060d301c1793684
Ella N <38847123+ellanan@users.noreply.github.com>
2022-02-03 00:36:00 -0500

    wrap app in Electron

---
+++ public/electron.js
@@ -0,0 +1,74 @@
+// reference https://dev.to/mandiwise/electron-apps-made-easy-with-create-react-app-and-electron-forge-560e
+
+// The app module, which controls your application's event lifecycle.
+// The BrowserWindow module, which creates and manages application windows.
+const { app, BrowserWindow } = require('electron');
+const isDev = require('electron-is-dev');
+
+const path = require('path');
+
+// Conditionally include the dev tools installer to load React Dev Tools
+let installExtension, REACT_DEVELOPER_TOOLS;
+
+if (isDev) {
+  const devTools = require('electron-devtools-installer');
+  installExtension = devTools.default;
+  REACT_DEVELOPER_TOOLS = devTools.REACT_DEVELOPER_TOOLS;
+}
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling
+if (require('electron-squirrel-startup')) {
+  app.quit();
+}
+
+// Add a createWindow() function that loads index.html into a new BrowserWindow instance.
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // and load the index.html of the app.
+  // win.loadFile("index.html");
+  win.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
+  );
+
+  // Open the DevTools.
+  if (isDev) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
+};
+
+// Manges window's lifecycle.
+// Quit the app when all windows are closed (Windows & Linux), except on macOS.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+// Open a window if none are open (macOS)
+app.whenReady().then(() => {
+  createWindow();
+
+  if (isDev) {
+    installExtension(REACT_DEVELOPER_TOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((error) => console.log(`An error occurred: , ${error}`));
+  }
+
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
